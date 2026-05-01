ee031ad2380dc953025f894cae428791b1513209
gasa-One <55610476+gasa-One@users.noreply.github.com>
2026-05-02 00:43:45 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import webmanifest from "astro-webmanifest";
 
-import netlify from "@astrojs/netlify";
+
 
 // https://astro.build/config
 export default defineConfig({
